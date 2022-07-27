610c805e3c4439bc6b1f6feec30eb31fd35ab03d
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-07-26 21:17:48 -0300

    exercicio bonus 2 do dia 4.3 feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio_bonus_1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio_bonus_1.js
@@ -1,10 +1,20 @@
-let tamanhoQuadrado = 5;
+let tamanhoQuadrado = 10;
 let linha;
 
-for(let index = 0; index < tamanhoQuadrado; index ++){
-  linha = "";
-  for(let index2 = 0; index2 < tamanhoQuadrado; index2++){
-    linha += "*  ";
+
+if(tamanhoQuadrado > 1){
+
+  for(let index = 0; index < tamanhoQuadrado; index ++){
+    
+    
+    linha = "";
+    for(let index2 = 0; index2 < tamanhoQuadrado; index2++){
+      linha += "*  ";
+    }
+    console.log(linha);
+    
   }
-  console.log(linha);
+
+}else{
+  console.log(tamanhoQuadrado+" não é um valor válido");
 }
