f063084e1548a76726c13634fef85eb9f075fcba
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-07-28 00:57:42 -0300

    exercicio 3 do dia 4.4 parte 2 feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio3DaParte2.js
@@ -0,0 +1,15 @@
+function maiorNumeroArray(array){ //função que retorna o maior valor encontrado em um array; 
+  let indexMenorNumero = 0;
+  for(let index = 1; index < array.length; index++){
+    
+    if(array[index] < array[indexMenorNumero]){
+      indexMenorNumero = index;
+    }
+  }
+
+  return indexMenorNumero;
+  
+}
+
+let test = [2, 4, 6, 7, 10, 0, -3];
+console.log(maiorNumeroArray(test));
