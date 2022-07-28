02f93f989a3247d78a7f97e88da90b6b681d1beb
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-07-28 20:50:00 -0300

    exercicio bonus 1 do dia 4.4 feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicioBonus1.js
@@ -0,0 +1,44 @@
+function romanToDecimal(romanNum){
+  romanNum = romanNum.toUpperCase();
+  let decimalNumber = 0;
+  let lastDecimalNumber = 0;
+  let somaDosAlgarismos = 0;
+
+  for(index = 0; index < romanNum.length; index++){
+    decimalNumber = retrieveDecimalAlgarism(romanNum[index]);
+
+    if(decimalNumber <= lastDecimalNumber){
+      somaDosAlgarismos += decimalNumber
+    }else{
+      somaDosAlgarismos -= lastDecimalNumber;
+      somaDosAlgarismos += decimalNumber-lastDecimalNumber;
+    }
+
+    lastDecimalNumber = decimalNumber;
+  }
+
+  return somaDosAlgarismos;
+}
+
+function retrieveDecimalAlgarism(romanAlg){
+  let romanAlgarisms = [];
+  let numToReturn = 0;
+
+  romanAlgarisms[1] = 'I';
+  romanAlgarisms[5] = 'V';
+  romanAlgarisms[10] = 'X';
+  romanAlgarisms[50] = 'L';
+  romanAlgarisms[100] = 'C';
+  romanAlgarisms[500] = 'D';
+  romanAlgarisms[1000] = 'M';
+
+  for(num in romanAlgarisms){
+    if(romanAlg == romanAlgarisms[num]){
+      numToReturn = num;
+    }
+  }
+
+  return parseInt(numToReturn);
+}
+
+console.log(romanToDecimal("xxiv"));
