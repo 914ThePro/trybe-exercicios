d2f655b2202d6ae08006950a32d34389a09ec669
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-08-03 01:49:01 -0300

    primeiro exercicio do dia 5.1 feito

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  const divs = document.getElementsByTagName("div");
+  const h3s = document.getElementsByTagName("h3");
+  const h1s = document.getElementsByTagName("h1");
+  const footer = document.getElementsByTagName("footer");
+
+  divs[0].style.backgroundColor = 'pink';
+  divs[1].style.backgroundColor = 'pink';
+
+  h3s[0].style.backgroundColor = 'magenta';
+  h3s[1].style.backgroundColor = 'magenta';
+
+  divs[2].style.backgroundColor = 'yellow';
+  divs[3].style.backgroundColor = 'yellow';
+
+  h3s[2].style.backgroundColor = 'black';
+  h3s[3].style.backgroundColor = 'black';
+
+  h1s[0].style.backgroundColor = 'green';
+  h1s[0].style.padding = "30px 0px";
+
+  footer[0].style.backgroundColor = 'green';
+}
