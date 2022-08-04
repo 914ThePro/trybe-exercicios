00616f7ad2d60de9b045ed48a0183410f3f18c90
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-08-04 10:59:42 -0300

    exercicio 5 do dia 5.3 feito

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -1,4 +1,5 @@
 let holidaysMarked = false;
+let fridaysMarked = false;
 
 function createDaysOfTheWeek() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
@@ -13,7 +14,7 @@ function createDaysOfTheWeek() {
   };
 };
 
-function creteDaysOfTheMonth(){
+function createDaysOfTheMonth(){
   let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
   13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   
@@ -73,21 +74,41 @@ function unmarkHolidays(){
 
 function createFridayButton(buttonName){
   let buttonsContainer = document.querySelector('.buttons-container');
-  let btnHoliday = document.createElement('button');
-  btnHoliday.innerText = buttonName;
-  buttonsContainer.appendChild(btnHoliday);
+  let btnFriday = document.createElement('button');
+  btnFriday.innerText = buttonName;
+  buttonsContainer.appendChild(btnFriday);
 
-  btnHoliday.addEventListener("click",function(){
-    if(holidaysMarked){
-      unmarkHolidays();
+  btnFriday.addEventListener("click",function(){
+    if(fridaysMarked){
+      unmarkFridays();
     }else{
-      markHolidays();
+      markFridays();
     }
   });
 }
 
+function markFridays(){
+  let fridays = document.querySelectorAll(".friday");
+
+  for(friday of fridays){
+    friday.innerText = "Sextou";
+  }
+  fridaysMarked = true;
+}
+
+function unmarkFridays(){
+  let fridays = document.querySelectorAll(".friday");
+  let acrecimoDias = 0;
+
+  for(friday of fridays){
+    friday.innerText = 4+acrecimoDias;
+    acrecimoDias += 7;
+  }
+  fridaysMarked = false;
+}
+
 createDaysOfTheWeek();
-creteDaysOfTheMonth();
+createDaysOfTheMonth();
 createHolidayButton('Feriados');
 createFridayButton('Sexta-feira');
 
