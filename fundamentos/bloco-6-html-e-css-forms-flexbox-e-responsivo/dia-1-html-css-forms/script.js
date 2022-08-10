c7b26983f8ce7f8578a9c16c204ee1178dce47e0
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-08-10 13:03:59 -0300

    exercicio bonus do dia 6.1 feito

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,7 +1,28 @@
-window.onload = function(){/*
- let erase = document.getElementById("erase");
+window.onload = function(){
+ let submit = document.getElementById("submit");
+ let nome = document.getElementById("inputName");
+ let email = document.getElementById("inputEmail");
+ let resposta = document.getElementById("inputResposta");
+ let dadosValidos = true;
 
- erase.addEventListener("click", function(e){
+ submit.addEventListener("click", function(e){
   e.preventDefault();
- });*/
+
+
+  if(nome.value.length < 10 || nome.value.length > 40) 
+    dadosValidos = false;
+
+  if(email.value.length < 10 || nome.value.length > 50)
+    dadosValidos = false;
+
+  if(resposta.value.length > 500)
+    dadosValidos = false;
+
+  if(dadosValidos)
+    alert("Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.");
+  else
+    alert("Dados Inválidos");
+
+  dadosValidos = true;
+ });
 }
