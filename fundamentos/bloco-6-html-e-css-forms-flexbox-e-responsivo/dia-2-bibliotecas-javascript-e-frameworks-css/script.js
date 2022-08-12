115c70b555153a6be182f6c21eb8d6cc645cc142
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-08-11 21:29:32 -0300

    exercicio bonus do dia 6.2 feitos

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -5,6 +5,64 @@ window.onload = function(){
  let resposta = document.getElementById("inputResposta");
  let dadosValidos = true;
 
+
+ let data = document.getElementById("inputDate");
+
+ data.DatePickerX.init({
+  mondayFirst: true,
+  minDate    : function()
+  {
+      var date = new Date();
+      return new Date().setDate(date.getDate() - 5);
+  },
+  maxDate    : function()
+  {
+      var date = new Date();
+      return new Date().setDate(date.getDate() + 10);
+  },
+  clearButton: false
+});
+
+
+
+
+ 
+ const validation = new JustValidate('#form');
+
+ validation
+ .addField('#inputName', [
+   {
+     rule: 'minLength',
+     value: 3,
+     errorMessage: 'O nome precisa ter no minimo 3 caracteres'
+   },
+   {
+     rule: 'maxLength',
+     value: 40,
+     errorMessage: 'O nome NÃO pode conter mais de 40 caracteres'
+   },
+   {
+    rule: 'required',
+    errorMessage:'É necessário informar seu nome'
+   }
+ ])
+ .addField("#inputEmail",[
+  {
+    rule: 'email',
+    errorMessage: 'Este e-mail não é valido'
+  }
+ ]).addField("#inputResposta",[
+  {
+  rule: 'maxLength',
+  errorMessage: 'A resposta deve ter no maximo 500 caracteres'
+  }
+ ]);
+
+
+
+
+
+/*
  submit.addEventListener("click", function(e){
   e.preventDefault();
 
@@ -24,5 +82,5 @@ window.onload = function(){
     alert("Dados Inválidos");
 
   dadosValidos = true;
- });
+ });*/
 }
