589493d43a0cfe310f407cf65d205091d018d029
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-08-26 09:38:10 -0300

    exercicio bonus parte 2 do dia 8.1 feito

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
@@ -28,11 +28,40 @@ const mageDamage = (intelligence, mana) => {
   let dano = Math.floor(intelligence + Math.random() * ((intelligence*2)-intelligence));
   let manaConsumida = 15;
   if(mana < 15){
-    console.log('Não possui mana suficiente');
+    dano = 'Não possui mana suficiente';
     manaConsumida = 0;
   }
   return {dano, manaConsumida};
 };
+
+const gameActions = {
+  warriorAtack : (damage) => {
+    let damageValue = damage(warrior.strength, warrior.weaponDmg);
+    dragon.healthPoints -= damageValue;
+    warrior.damage = damageValue;
+  },
+  mageAtack: (damage) => {
+    let damageValue = damage(mage.intelligence, mage.mana);
+    dragon.healthPoints -= damageValue.dano;
+    mage.mana -= damageValue.manaConsumida;
+    mage.damage = damageValue.dano;
+  },
+  dragonAtack(damage){
+    let damageValue = damage(dragon.strength);
+    warrior.healthPoints -= damageValue;
+    mage.healthPoints -= damageValue;
+    dragon.damage = damageValue;
+  },
+  makeTurn: () => {
+    gameActions.dragonAtack(dragonDamage);
+    gameActions.mageAtack(mageDamage);
+    gameActions.warriorAtack(warriorDamage);
+    return battleMembers;
+  }
+  
+};
+
+console.log(gameActions.makeTurn());
 
 
 
