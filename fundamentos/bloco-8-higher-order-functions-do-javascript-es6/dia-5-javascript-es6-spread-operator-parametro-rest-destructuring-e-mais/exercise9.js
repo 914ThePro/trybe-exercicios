86f95bf75c5fc6d012f2acb00efc7c0078fa2308
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-08-31 21:37:48 -0300

    exercicio 9 do dia 8.5 feito

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise9.js
@@ -0,0 +1,12 @@
+// yearSeasons: estações do ano.
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+
+const {spring, summer, autumn, winter} = yearSeasons;
+const yearMonths = [...spring, ...summer, ...autumn, ...winter];
+
+console.log(yearMonths);
